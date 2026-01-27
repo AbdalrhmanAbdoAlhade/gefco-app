4df1e3591b58ffb0e3c82acbc4617f94a90702a9
Abdalrhman abdalnabe <161177575+AbdalrhmanAbdoAlhade@users.noreply.github.com>
2026-01-27 14:30:32 +0200

    Update Equipment.php

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Equipment extends Model
 {
     use HasFactory;
-
+        
+    protected $table = 'equipments';
+    
     protected $fillable = [
         'name',
         'image',
