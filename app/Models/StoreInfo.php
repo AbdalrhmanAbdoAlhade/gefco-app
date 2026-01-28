2a20b574ed330f71d485dd58d292b4f2ed0423ee
Abdalrhman abdalnabe <161177575+AbdalrhmanAbdoAlhade@users.noreply.github.com>
2026-01-28 13:05:29 +0200

    Update StoreInfo.php

--- app/Models/StoreInfo.php
+++ app/Models/StoreInfo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class StoreInfo extends Model
 {
-    protected $fillable = ['name', 'logo', 'phone', 'email', 'address', 'social_links'];
+    protected $fillable = ['name', 'logo', 'phone', 'email', 'completed_projects', 'satisfied_clients', 'experience', 'address', 'social_links'];
 
     protected $casts = [
         'social_links' => 'array',
